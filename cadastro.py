de6873331977112fcd168e5a11a8383edd6b108a
arthur <arthursousere@gmail.com>
2026-05-11 08:46:18 -0300

    atividade 5

---
+++ cadastro.py
@@ -0,0 +1,21 @@
+class pessoa:
+ def __init__(self, nome, email, sexo, numero, cpf):
+    self.nome = nome
+    self.email = email
+    self.sexo = sexo
+    self.numero = numero
+    self.cpf = cpf
+
+p1=pessoa("arthur","arthursousere@gmail.com", "masculino", "61981578556", "08558364150")
+p2=pessoa("isabelly", "bebelly@gmail.com", "feminino", "6195517249", "11122233345")
+print(p1.nome)
+print(p1.emeil)
+print(p1.sexo)
+print(p1.numero)
+print(p1.cpf)
+print
+(p2.nome)
+print(p2.emeil)
+print(p2.sexo)
+print(p2.numero)
+print(p2.cpf)
